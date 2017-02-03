c71a6f82364c39d576a6a3e3b2b3b2ed72c97fe1
Вячеслав Епанча < >
2017-02-03 14:37:44 +0300

    Add files

---
+++ src/controllers/DownloadController.php
@@ -0,0 +1,24 @@
+<?php namespace Yunicot\Laravelfilemanager\controllers;
+
+use Yunicot\Laravelfilemanager\controllers\Controller;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
+
+/**
+ * Class DownloadController
+ * @package Unisharp\Laravelfilemanager\controllers
+ */
+class DownloadController extends LfmController {
+
+    /**
+     * Download a file
+     *
+     * @return mixed
+     */
+    public function getDownload()
+    {
+        return Response::download(parent::getPath('directory') . Input::get('file'));
+    }
+
+}
